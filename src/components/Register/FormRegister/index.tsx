5febd4dc157f5117974297ca6cf43aafe5e7f047
jvolima <jvolima2004@gmail.com>
2022-03-16 21:28:05 -0300

    Consumindo back-end e começando a conectar o form com o react-hook-form

--- src/components/Register/FormRegister/index.tsx
+++ src/components/Register/FormRegister/index.tsx
@@ -1,46 +1,75 @@
 import Link from "next/link";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import 'react-toastify/dist/ReactToastify.min.css';
 import { FormEvent, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { api } from "../../../services/api";
 import { Button } from "../../FormElements/Button";
 import { Input } from "../../FormElements/Input";
 import { Container } from "./styles";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-export function FormRegister() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+interface CreateUserFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
-  function handleRegister(event: FormEvent): void {
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  email: yup.string().required('Email is required').email('Invalid email'),
+  password: yup.string().required('Password is required').min(6, 'At least 6 caracters')
+});
+
+export const FormRegister = () => {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema)
+  })
+
+  const handleRegister: SubmitHandler<CreateUserFormData> = async (data, event: FormEvent) => {
+    console.log(data)
     event.preventDefault();
 
-    console.log({
-      name,
-      email, 
-      password
-    })
+    try {
+      const response = await api.post("/users", {
+        data
+      });
+
+      console.log(response)
+    } catch (err) {
+      toast.error("An user with this email already exists!");
+    }
   }
 
   return (
     <Container>
+      <ToastContainer 
+        theme="colored" 
+        toastClassName="errorAlert"
+        autoClose={3000} 
+        pauseOnHover={false} 
+      />
       <h1>Criar sua conta no devshare</h1>
-      <form onSubmit={handleRegister}>
+      <form onSubmit={handleSubmit(handleRegister)}>
         <Input 
           type="text" 
           name="name" 
-          onChange={data => setName(data.target.value)}
           placeholder="Nome"
+          {...register('name')}
         />
         <Input 
           type="email" 
           name="email" 
-          onChange={data => setEmail(data.target.value)}
           placeholder="E-mail"
+          {...register('email')}
         />
         <Input 
           type="password" 
           name="password" 
-          onChange={data => setPassword(data.target.value)}
           placeholder="Senha"
           autoComplete="on"
+          {...register('password')}
         />
 
         <Button>Cadastrar</Button>
